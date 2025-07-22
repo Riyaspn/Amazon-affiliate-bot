26112608ee0435562ce07ff4ed782d19ec7a9efc
Riyaspn <111150254+Riyaspn@users.noreply.github.com>
2025-07-23 04:07:31 +0530

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -213,8 +213,9 @@ import re
 def escape_markdown(text: str) -> str:
     if not text:
         return ""
-    escape_chars = r"\_*[]()~`>#+-=|{}.!"
+    escape_chars = r'\_*[]()~`>#+-=|{}.!'
     return ''.join(['\\' + c if c in escape_chars else c for c in text])
+
 
 
 
